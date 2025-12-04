ef2735ebd66f869d4fee3f0440b20eb133f84610
Syed Hunzala Ahmed Bukhari <k253089@nu.edu.pk>
2025-12-05 02:31:11 +0500

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+void rotate(int n, int matrix[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n/2; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[i][n - 1 - j];
+            matrix[i][n - 1 - j] = temp;
+        }
+    }
+}
+
+void printMatrix(int n, int matrix[n][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int matrix[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int n = 3;
+
+    rotate(n, matrix);
+    printMatrix(n, matrix);
+
+    return 0;
+}
